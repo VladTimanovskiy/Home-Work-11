be367006443ef630a450d3339f15c6ac8e9bf18b
VladTimanovskiy <lonelywoolf2015@gmail.com>
2024-07-15 16:19:03 +0300

    refactor Task5

--- Task5/StreamUtils.java
+++ Task5/StreamUtils.java
@@ -1,32 +1,18 @@
 package module11.Task5;
 
 import java.util.Iterator;
-import java.util.Spliterator;
-import java.util.Spliterators;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 public class StreamUtils {
     public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
         Iterator<T> iterator1 = first.iterator();
         Iterator<T> iterator2 = second.iterator();
 
-        Iterator<T> zippedIterator = new Iterator<T>() {
-            @Override
-            public boolean hasNext() {
-                return iterator1.hasNext() && iterator2.hasNext();
-            }
-
-            @Override
-            public T next() {
-                return iterator1.next();
-            }
-        };
-
-        Spliterator<T> spliterator = Spliterators.spliteratorUnknownSize(zippedIterator, Spliterator.ORDERED);
-
-        return StreamSupport.stream(spliterator, false)
-                .flatMap(item -> Stream.of(item, iterator2.next()));
+        return IntStream.iterate(0, i -> i + 1)
+                .boxed()
+                .takeWhile(i -> iterator1.hasNext() && iterator2.hasNext())
+                .flatMap(i -> Stream.of(iterator1.next(), iterator2.next()));
     }
 
     public static void main(String[] args) {
